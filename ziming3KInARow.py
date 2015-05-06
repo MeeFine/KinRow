2259026986f8868c19a90261e3fdef4a3f47b424
Ziming Guo <ziming3@ougl-66-d.eplt.washington.edu>
2015-05-05 22:08:28 -0700

    zobristnum added

--- ziming3KInARow.py
+++ ziming3KInARow.py
@@ -2,47 +2,50 @@ from random import randint
 
 hi = 0
 wi = 0
+forbid = []
 k = 0
 side = ''
 opponent = ''
 zobristnum = []
-
 def prepare(initial_state, k, what_side_I_play, opponent_nick_name):
     global zobristnum
     glob = globals()
     glob['k'] = k
     glob['side'] = what_side_I_play
     glob['opponent'] = opponent_nick_name
-    global hi, wi
-    hi = len(initial_state[0])
-    wi = len(initial_state[0][0])
     board = initial_state[0]
-    blank = 0
-    for row in board:
-        for i in row:
-            if i != "-":
-                blank += 1
-    zinit(blank)
+    global hi, wi, forbid
+    hi = len(board)
+    wi = len(board[0])
+    for i in range(hi):
+        for j in range(wi):
+            if i == "-":
+                forbid.append((i, j))
+    zinit()
     return "OK"
 
 
-def zinit(blank):
-    global zobristnum
-    zobristnum = [[0] * 2] * blank
-    for i in range(blank):
-        for j in range(2):
-            zobristnum[i][j] = randint(0, 4294967296)
+def zinit():
+    global zobristnum, hi, wi, forbid
+    zobristnum = [[[0] * 2] * wi] * hi
+    for i in range(hi):
+        for j in range(wi):
+            if (i, j) not in forbid:
+                for k in range(2):
+                    zobristnum[i][j][k] = randint(0, 4294967296)
 
 
-def zhash(s, blank):
-    global zobristnum
+def zhash(board):
+    global zobristnum, hi, wi, forbid
     val = 0
-    for i in range(blank):
-        piece = None
-        if s[i] == "X" : piece = 0
-        if s[i] == "O" : piece = 1
-        if piece != None:
-            val ^= zobristnum[i][piece]
+    for i in range(hi):
+        for j in range(wi):
+            if (i, j) not in forbid:
+                piece = None
+                if board[i][j] == "X" : piece = 0
+                if board[i][j] == "O" : piece = 1
+                if piece != None:
+                    val ^= zobristnum[i][j][piece]
     return val
 
 
