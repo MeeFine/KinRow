287a585d0289a6ddad1e0af52e60230da8a5dcd8
Ziming Guo <ziming3@ougl-66-d.eplt.washington.edu>
2015-05-05 21:45:28 -0700

    Correction on state

--- ziming3KInARow.py
+++ ziming3KInARow.py
@@ -1,15 +1,50 @@
-initial_state = []
+from random import randint
+
+hi = 0
+wi = 0
 k = 0
 side = ''
 opponent = ''
+zobristnum = []
+
 def prepare(initial_state, k, what_side_I_play, opponent_nick_name):
+    global zobristnum
     glob = globals()
-    glob['initial_state'] = initial_state
     glob['k'] = k
     glob['side'] = what_side_I_play
     glob['opponent'] = opponent_nick_name
-
+    global hi, wi
+    hi = len(initial_state[0])
+    wi = len(initial_state[0][0])
+    board = initial_state[0]
+    blank = 0
+    for row in board:
+        for i in row:
+            if i != "-":
+                blank += 1
+    zinit(blank)
     return "OK"
+
+
+def zinit(blank):
+    global zobristnum
+    zobristnum = [[0] * 2] * blank
+    for i in range(blank):
+        for j in range(2):
+            zobristnum[i][j] = randint(0, 4294967296)
+
+
+def zhash(s, blank):
+    global zobristnum
+    val = 0
+    for i in range(blank):
+        piece = None
+        if s[i] == "X" : piece = 0
+        if s[i] == "O" : piece = 1
+        if piece != None:
+            val ^= zobristnum[i][piece]
+    return val
+
 
 def introduce():
     return '''
